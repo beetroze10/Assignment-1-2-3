9cc3647df52e03f387b83a10685e304a5536d015
beetroze10 <boomingbeetroze@gmail.com>
2023-11-10 14:48:27 +0300

    complete assign1

---
+++ StudentInfo.java
@@ -0,0 +1,19 @@
+public class StudentInfo {
+
+    
+    public static void main(String[] args) {
+         String course = "Computer Science"; 
+    String name = "John Doe";
+    String dob = "2000-01-25"; 
+    String vision = "future";
+    String favLang = "Java";
+
+    System.out.println("Course: " + course);
+    System.out.println("Name: " + name);
+    System.out.println("Date of Birth: " + dob);
+    System.out.println("Vision: " + vision);
+    System.out.println("Favorite Language: " + favLang);
+
+    }
+    
+}
